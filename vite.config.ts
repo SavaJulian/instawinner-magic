3fd0c5271407563ad32b7673c26986db68d3128e
SavaJulian <126976237+SavaJulian@users.noreply.github.com>
2026-05-11 18:12:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+});import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+export default defineConfig({
+    vite: {
+          base: "/instawinner-magic/",
+    },
+    tanstackStart: {
+          server: { entry: "server" },
+    },
 });
